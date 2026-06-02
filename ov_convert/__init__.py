9ca4548c9ef789ca72176a7d70532a6534328b9a
Michael Carroll <mjoecarroll@gmail.com>
2026-06-02 15:55:33 -0400

    Add export to... exports

--- ov_convert/__init__.py
+++ ov_convert/__init__.py
@@ -2,9 +2,10 @@
 
 from src import models
 
-from .convert import export_from_config_file
+from .convert import export, export_from_config_file
 
 __all__ = (
+  "export",
   "export_from_config_file",
   "models",
 )
