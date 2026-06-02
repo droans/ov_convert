4c76185b01070f5e8a9066d3185d3ccecc235165
Michael Carroll <mjoecarroll@gmail.com>
2026-06-02 15:53:52 -0400

    Export model schemas

--- ov_convert/models/__init__.py
+++ ov_convert/models/__init__.py
@@ -1,8 +1,28 @@
 """Models used by script."""
 
+from .export import LlmModelExportSettingsConfigSchema, VlmModelExportSettingsConfigSchema
+from .load_options import LoadOptions
 from .model import LlmModelConfiguration, VlmModelConfiguration
+from .model_information import LlmModelInformationConfig, VlmModelInformationConfig
+from .quantization import (
+  FullQuantizationConfig,
+  IgnoredScopeConfig,
+  LlmQuantizationSettingsSchema,
+  VlmQuantizationSettingsSchema,
+  WeightQuantizationConfig,
+)
 
 __all__ = [
+  "FullQuantizationConfig",
+  "IgnoredScopeConfig",
   "LlmModelConfiguration",
+  "LlmModelExportSettingsConfigSchema",
+  "LlmModelInformationConfig",
+  "LlmQuantizationSettingsSchema",
+  "LoadOptions",
   "VlmModelConfiguration",
+  "VlmModelExportSettingsConfigSchema",
+  "VlmModelInformationConfig",
+  "VlmQuantizationSettingsSchema",
+  "WeightQuantizationConfig",
 ]
