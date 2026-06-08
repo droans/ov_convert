4a39b369f8d8fe628ef3cf9f466310438c61480b
Michael Carroll <mjoecarroll@gmail.com>
2026-06-08 13:00:08 -0400

    Add models to support dependency management

--- ov_convert/cli/model.py
+++ ov_convert/cli/model.py
@@ -1,10 +1,18 @@
 """CLI Argument Model."""
 
-from pydantic import BaseModel
+from typing import Literal
+
+from pydantic import AliasChoices, BaseModel, Field
 from pydantic_settings import (
     BaseSettings,
     CliPositionalArg,
     CliSubCommand,
+)
+
+from ov_convert.cli.const import (
+    AptDependencyGroupsType,
+    PipDependencyGroupOptionsType,
+    PipDependencyOptionsType,
 )
 
 
@@ -12,6 +20,26 @@ class ConvertModelSchema(BaseModel):
     """Convert OV model: Config schema."""
 
     config_path: CliPositionalArg[str]
+
+
+class DependencyManagementModelSchema(BaseModel):
+    """Dependency Management: Install/update dependencies."""
+
+    action: CliPositionalArg[Literal["install", "upgrade"]]
+    dependency: CliPositionalArg[
+        Literal["all"]
+        | AptDependencyGroupsType
+        | PipDependencyOptionsType
+        | PipDependencyGroupOptionsType
+    ]
+    branch: str | None = Field(
+        default=None,
+        alias=AliasChoices("b", "branch"),
+    )  # ty:ignore[no-matching-overload]
+    pr: int | None = Field(
+        default=None,
+        alias=AliasChoices("pr", "pull-request"),
+    )  # ty:ignore[no-matching-overload]
 
 
 class CLIModelSchema(BaseSettings, cli_parse_args=True, cli_exit_on_error=False):
