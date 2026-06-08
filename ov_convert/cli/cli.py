edceb29dacf2b678687c26fa97cc18389ab5c95c
Michael Carroll <mjoecarroll@gmail.com>
2026-06-08 13:04:28 -0400

    Add functions to CLI call

--- ov_convert/cli/cli.py
+++ ov_convert/cli/cli.py
@@ -7,10 +7,12 @@ from pydantic import BaseModel
 from pydantic_settings import get_subcommand
 
 from ov_convert.cli.convert import cli_convert
-from ov_convert.cli.model import CLIModelSchema, ConvertModelSchema
+from ov_convert.cli.deps import cli_manage_dependencies
+from ov_convert.cli.model import CLIModelSchema, ConvertModelSchema, DependencyManagementModelSchema
 from ov_convert.cli.util import print_help
 
 SUBCOMMAND_FUNCS: dict[type[BaseModel], Callable] = {
+    DependencyManagementModelSchema: cli_manage_dependencies,
     ConvertModelSchema: cli_convert,
 }
 
@@ -18,7 +20,6 @@ SUBCOMMAND_FUNCS: dict[type[BaseModel], Callable] = {
 def cli_call() -> None:
     """Handle `ov-convert` shell requests."""
     args = sys.argv
-    print(args)
     if "-h" in args or "--help" in args or len(args) < 2:  # noqa: PLR2004
         print_help()
     if len(sys.argv) > 2:  # noqa: PLR2004
