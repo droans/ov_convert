d4b1c85a3af23573f8a5a6573ebc44d42414fe26
Michael Carroll <mjoecarroll@gmail.com>
2026-06-08 13:04:18 -0400

    Add functions to manage dependencies

--- ov_convert/cli/util.py
+++ ov_convert/cli/util.py
@@ -1,6 +1,18 @@
 """CLI Utility functions."""
 
+import os
 import shutil
+import subprocess
+import sys
+from typing import Literal
+
+from ov_convert.cli.const import (
+    APT_GROUP_DEPENDENCIES,
+    PIP_GROUP_DEPENDENCIES,
+    AptDependencyGroupsType,
+    PipDependencyGroupOptionsType,
+    PipDependencyOptionsType,
+)
 
 
 def print_help() -> None:
@@ -50,3 +62,96 @@ Parameters:
             -pr, --pull-request     Specify a specific pull request to use for
                                     the dependency
 """)
+
+
+def get_bin_path(bin_name: str, error_out: bool = True) -> str:  # noqa: FBT001, FBT002
+    """Get the path for a binary using `which {bin}`."""
+    cmd_result = subprocess.run(["which", bin_name], check=True, capture_output=True)  # noqa: S603, S607
+    if cmd_result.returncode != 0:
+        msg = f"Could not get binary for {bin_name} (Return code: {cmd_result.returncode})"
+        if error_out:
+            raise SystemError(msg)
+        print(f"Received error: {msg}")
+        return ""
+    return cmd_result.stdout.decode().replace("\n", "")
+
+
+def is_sudo_present() -> bool:
+    """Test if `sudo` is present on the machine."""
+    path = get_bin_path("sudo")
+    return len(path) > 0
+
+
+def get_pip_env_cmd() -> list[str]:
+    """Get the proper pip command based on the presence of the venv."""
+    if "OPENARC_API_KEY" in os.environ:
+        # OpenArc - use default `uv` executables.
+        uv_path = get_bin_path("uv")
+        cmd = [
+            uv_path,
+            "--directory",
+            "/app",
+        ]
+    else:
+        cmd = [sys.executable, "-m"]
+    return [
+        *cmd,
+        "pip",
+        "install",
+    ]
+
+
+def install_apt_dependency(
+    dep: str | list[str],
+    install_or_upgrade: Literal["install", "upgrade"],
+) -> None:
+    """Installs a single or multiple apt dependencies."""
+    if isinstance(dep, list):
+        dep = " ".join(dep)
+    update_cmd: list[str] = []
+    cmd: list[str] = []
+    if is_sudo_present():
+        print("sudo found on machine. You may be asked to elevate privileges below.")
+        update_cmd.append("sudo")
+        cmd.append("sudo")
+    cmd += ["apt-get", "install"]
+    if install_or_upgrade == "upgrade":
+        cmd += "--upgrade"
+    cmd += ["-y", dep]
+    update_cmd += ["apt-get", "upgrade"]
+    print(f"Calling command: {update_cmd}")
+    subprocess.run(update_cmd, check=True, shell=False)  # noqa: S603
+    print(f"Calling command: {cmd}")
+    subprocess.run(cmd, check=True, shell=False)  # noqa: S603
+
+
+def install_apt_group_dependency(
+    dependency_group: AptDependencyGroupsType,
+    install_or_upgrade: Literal["install", "upgrade"],
+) -> None:
+    """Installs all dependencies for an apt dependency group."""
+    all_deps = APT_GROUP_DEPENDENCIES[dependency_group]
+    install_apt_dependency(all_deps, install_or_upgrade)
+
+
+def install_pip_dependency(
+    dep: str | list[str] | PipDependencyOptionsType | list[PipDependencyOptionsType],
+    install_or_upgrade: Literal["install", "upgrade"],
+) -> None:
+    """Installs a single or multiple apt dependencies."""
+    used_dep = " ".join(dep) if isinstance(dep, list) else str(dep)
+    cmd = get_pip_env_cmd()
+    if install_or_upgrade == "upgrade":
+        cmd += ["--upgrade"]
+    cmd += [used_dep]
+    print(f"Calling command: {cmd}")
+    subprocess.run(cmd, check=True, shell=False)  # noqa: S603
+
+
+def install_pip_group_dependency(
+    dependency_group: PipDependencyGroupOptionsType,
+    install_or_upgrade: Literal["install", "upgrade"],
+) -> None:
+    """Installs all dependencies for a pip dependency group."""
+    dependencies = PIP_GROUP_DEPENDENCIES[dependency_group]
+    install_pip_dependency(dependencies, install_or_upgrade)
